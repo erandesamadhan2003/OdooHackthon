e0c264cd364d9a2c900444dcfe8f5e959a3cef27
erandesamadhan2003 <erandesamadhan2003@gmail.com>
2025-07-12 09:54:29 +0530

    local changes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv'
+dotenv.config();
 
 // Register
 export const register = async (req, res) => {
--- backend/db/connect.js
+++ backend/db/connect.js
@@ -4,7 +4,8 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb+srv://erandesamadhan2003:Samadhan@cluster0.evww6jb.mongodb.net/`); // This should not be undefined
+    // const conn = await mongoose.connect(`mongodb+srv://erandesamadhan2003:Samadhan@cluster0.evww6jb.mongodb.net/`); // This should not be undefined
+     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
