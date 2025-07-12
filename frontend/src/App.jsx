4d200037c084da0a4e4947d9b87124460c6d7eab
erandesamadhan2003 <erandesamadhan2003@gmail.com>
2025-07-12 08:42:17 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-700 text-3xl'>
+        Samadhan
+      </div>
+    
+    </>
+  )
+}
+
+export default App
