6e0b1180757608f0ed7579d543550246886ab274
harshaagrawal20 <harshaagarwal820@gmail.com>
2025-07-12 10:32:34 +0530

    authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
 import './App.css'
 import { Home } from './pages/Home'
 import { Login } from './pages/auth/Login'
 import { Signup } from './pages/auth/Signup'
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 const router = createBrowserRouter([
   {
@@ -21,10 +29,9 @@ const router = createBrowserRouter([
 const App = () => {
 
   return (
-    <>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <RouterProvider router={router}></RouterProvider>
-    
-    </>
+    </ClerkProvider>
   )
 }
 
